f36df912e2d30b1f8e1044d1bae4f50e7ffdd4b7
Tim Menzies <tim.menzies@gmail.com>
2017-07-05 05:13:23 +1000

    saving

--- chop.lua
+++ chop.lua
@@ -47,53 +47,55 @@ require "show"
 local the=require "config"
 local num=require "num"
 ----------------------------------
-local function create(lst, x)  
+local function range() return {
+  all= {},
+  n  = 0,
+  hi = -2^63,
+  lo =  2^63,
+  span = 2^64} end
+----------------------------------
+local function updateRange(i,one, x1)
+  i.all[#i.all+1] = one
+  i.n = i.n + 1
+  if x1 > i.hi then 
+    i.hi = x1 
+    i.span = i.hi - i.lo
+  else 
+    if x1 < i.lo then 
+      i.lo = x1 
+      i.span = i.hi - i.lo end end end  
+----------------------------------
+local function nextRange(i) 
+  i.now  = range()
+  i.ranges[#i.ranges+1] = i.now end
+----------------------------------
+local function rangeMaker(lst, x)  
   local _= { 
     x     = x or function (z) return z  end,
     cohen = the.chop.cohen,
     m     = the.chop.m,
     size  = #lst,
     ranges= {} }
-  table.sort(lst,_.x)
+  nextRange(_)
   local n = num.updates(lst, _.x)
   _.enough = _.size^_.m
   _.hi     = n.hi
   _.epsilon= n.sd * _.cohen
-  return _ end
-----------------------------------
-local function range() return {
-  all= {},
-  hi = -2^63,
-  lo =  2^63,
-  span = 2^64,
-  n  =  0 }  end
-----------------------------------
-local function update(i,one,x) 
-  if x > i.hi then i.hi= x end 
-  if x < i.lo then i.lo= x end 
-  i.all[#i.all+1] = one
-  i.n = i.n + 1
-  i.span = i.hi - i.lo
-end  
-----------------------------------
-local function nextRange(i) 
-  local _ = range()
-  i.ranges[#i.ranges+1] = _
+  table.sort(lst,_.x)
   return _ end
 ----------------------------------
 return function (lst, x,       last)
-  local i= create(lst, x)
-  local r= nextRange(i)
+  local i= rangeMaker(lst, x)
   for j,one in pairs(lst) do
     local x1 = x(one)
-    update(r, one, x1)
+    updateRange(i.now, one, x1)
     if j > 1 and
-       r.n        > i.enough  and
-       r.span     > i.epsilon and
-       i.size - j > i.enough  and
-       i.hi  - x1 > i.epsilon and
-       x1  - last > i.epsilon 
-    then r = nextRange(i) end 
+       i.now.n        > i.enough  and
+       i.now.span     > i.epsilon and
+       i.now.n   -  j > i.enough  and
+       i.now.hi  - x1 > i.epsilon and
+       x1      - last > i.epsilon 
+    then nextRange(i) end 
     last = x1 
   end 
-  return i end
+  return i.ranges end
