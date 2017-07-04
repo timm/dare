94414b2213a6e3673672b89c5e83f750eb9a1b76
Tim Menzies <tim.menzies@gmail.com>
2017-07-04 20:30:05 +1000

    saving

--- chop.lua
+++ chop.lua
@@ -46,15 +46,54 @@ POSSIBILITY OF SUCH DAMAGE.
 require "show"
 local the=require "config"
 local num=require "num"
-
+----------------------------------
 local function create(lst, x)  
-  local _= { x     = x or function (z) return z  end,
-             cohen = the.chop.cohen,
-             m     = the.chop.m,
-             size  = #lst }
-  table.sort(lst,i _.x)
+  local _= { 
+    x     = x or function (z) return z  end,
+    cohen = the.chop.cohen,
+    m     = the.chop.m,
+    size  = #lst,
+    ranges= {} }
+  table.sort(lst,_.x)
   local n = num.updates(lst, _.x)
   _.enough = _.size^_.m
   _.hi     = n.hi
   _.epsilon= n.sd * _.cohen
   return _ end
+----------------------------------
+local function range() return {
+  all= {},
+  hi = -2^63,
+  lo =  2^63,
+  span = 2^64,
+  n  =  0 }  end
+----------------------------------
+local function update(i,one,x) 
+  if x > i.hi then i.hi= x end 
+  if x < i.lo then i.lo= x end 
+  i.all[#i.all+1] = one
+  i.n = i.n + 1
+  i.span = i.hi - i.lo
+end  
+----------------------------------
+local function nextRange(i) 
+  local _ = range()
+  i.ranges[#i.ranges+1] = _
+  return _ end
+----------------------------------
+return function (lst, x,       last)
+  local i= create(lst, x)
+  local r= nextRange(i)
+  for j,one in pairs(lst) do
+    local x1 = x(one)
+    update(r, one, x1)
+    if j > 1 and
+       r.n        > i.enough  and
+       r.span     > i.epsilon and
+       i.size - j > i.enough  and
+       i.hi  - x1 > i.epsilon and
+       x1  - last > i.epsilon 
+    then r = nextRange(i) end 
+    last = x1 
+  end 
+  return i end
